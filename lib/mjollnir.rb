8bc8662f24b81f52e1ba1fb876d309d9da2ad498
DaveTD <its.dave.dawson@gmail.com>
2012-10-10 01:14:27 -0400

    Gem now checks the existance of each file separately and does not fail if any do not exist.

--- lib/mjollnir.rb
+++ lib/mjollnir.rb
@@ -1,35 +1,33 @@
 require "mjollnir/version"
 
 module Mjollnir
-	def self.include(base)
-		base.extend(ClassMethods)
-	end
-
 	def template(source, *args, &block)
 		inside_template do
 			super
-			if File.exists?("lib/mjollnir") && File.directory?("lib/mjollnir")
-				target_file = args.first|| source.sub(/\.tt$/, '')
-
-				prependable = ERB.new(::File.binread("lib/mjollnir/prepend.rb"), nil, '-', '@output_buffer')
-				appendable = ERB.new(::File.binread("lib/mjollnir/append.rb"), nil, '-', '@output_buffer')
 			
+			mjollnir_root = "lib/mjollnir"
+
+			if File.exists?(mjollnir_root) && File.directory?(mjollnir_root)
+				target_file = args.first|| source.sub(/\.tt$/, '')
 				target_file_type = target_file.split('.').pop
-
-				commentstarts = Hash[*File.read('lib/mjollnir/comment_starts.csv').split(/[, \n]+/)]
-				commentends = Hash[*File.read('lib/mjollnir/comment_ends.csv').split(/[, \n]+/)]
-				universal_comment_start = commentstarts[target_file_type] 
-				universal_comment_end = commentends[target_file_type]	
-
-				# Bind the variables to the ERB results
-				preargs = [
-					prependable.result(binding)
-				]
-				postargs = [
-					appendable.result(binding)
-				]
-				prepend_to_file(target_file, *preargs, &block)
-				append_to_file(target_file, *postargs, &block)
+		
+				if File.exists?(mjollnir_root + "/comment_starts.csv")
+					commentstarts = Hash[*File.read(mjollnir_root + '/comment_starts.csv').split(/[, \n]+/)]
+					universal_comment_start = commentstarts[target_file_type] 
+				end
+				if File.exists?(mjollnir_root + "/comment_ends.csv")
+					commentends = Hash[*File.read(mjollnir_root + '/comment_ends.csv').split(/[, \n]+/)]
+					universal_comment_end = commentends[target_file_type]	
+				end
+			
+				if File.exists?(mjollnir_root + "/prepend.rb") 
+					preargs = [ ERB.new(::File.binread(mjollnir_root + "/prepend.rb"), nil, '-', '@output_buffer').result(binding) ]
+					prepend_to_file(target_file, *preargs, &block)
+				end
+				if File.exists?(mjollnir_root + "/append.rb")
+					postargs = [ ERB.new(::File.binread(mjollnir_root + "/append.rb"), nil, '-', '@output_buffer').result(binding) ]
+					append_to_file(target_file, *postargs, &block)
+				end
 			end
 		end
 	end
--- lib/mjollnir/version.rb
+++ lib/mjollnir/version.rb
@@ -1,3 +1,3 @@
 module Mjollnir
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
