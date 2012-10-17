40e56ee3daa63fa6df15b9763d298d503bae114c
DaveTD <its.dave.dawson@gmail.com>
2012-10-16 21:50:37 -0400

    Updating with templates

--- lib/generators/mjollnir/install_generator.rb
+++ lib/generators/mjollnir/install_generator.rb
@@ -1,14 +1,14 @@
 module Mjollnir
   module Generators
-	class MjollnirGenerator < Rails::Generators::Base
-		source_root File.expand_path("../../templates", __FILE__)
-
+	class InstallGenerator < Rails::Generators::Base
 		desc "Creates the four required files for mjollnir"
 
+		source_root File.expand_path("../../templates", __FILE__)
+
 		def create_lib_files
-			empty_directory "lib/mjollnir"
-		
 			mjollnir_root = "lib/mjollnir"
+	
+			empty_directory mjollnir_root	
 		
 			create_file mjollnir_root + "/prepend.rb", <<-FILE
 
@@ -16,26 +16,11 @@ module Mjollnir
 			create_file mjollnir_root + "/append.rb", <<-FILE
 
 			FILE
-			create_file mjollnir_root + "/comment_starts.csv", <<-FILE
-rb,=begin
-scss,/*
-js,/*
-haml,#-
-coffee,###
-css,/*
-erb,<!--
-html,<!--
-yml,#
-			FILE
-			create_file mjollnir_root + "/comment_ends.csv", <<-FILE
-rb,=end
-scss,*/
-js,*/
-coffee,###
-css,*/
-erb,-->
-html,-->
-			FILE
+			#create_file mjollnir_root + "/comment_starts.csv", File.join('comment_starts.csv')
+			template 'comment_starts.csv', File.join(mjollnir_root, 'comment_starts.csv')
+
+			#create_file mjollnir_root + "/comment_ends.csv", File.join('comment_ends.csv')
+			template 'comment_ends.csv', File.join(mjollnir_root, 'comment_ends.csv')
 		end
 	end
   end
